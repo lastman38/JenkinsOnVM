0b4abedb988c70f7a42800e494b63098e1fa4525
lastman38 <61261700+lastman38@users.noreply.github.com>
2020-06-22 14:35:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like IU4!');
+    res.send('I like IU5!');
 });
 
 app.get('/start', function(req, res){
